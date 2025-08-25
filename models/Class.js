a0e260a51918ac1504fd987bfe3f265f9f0a1003
Krish2654 <itskrish2654+4@gmail.com>
2025-08-25 19:23:36 +0530

    sync

--- models/Class.js
+++ models/Class.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 
 const classSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 100
+  },
   subject: {
     type: String,
     required: true,
@@ -12,16 +18,22 @@ const classSchema = new mongoose.Schema({
     trim: true,
     maxlength: 500
   },
-  classGroup: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'ClassGroup',
-    required: true
+  classCode: {
+    type: String,
+    required: true,
+    unique: true,
+    uppercase: true,
+    length: 6
   },
   teacher: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true
   },
+  students: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
   isActive: {
     type: Boolean,
     default: true
@@ -34,6 +46,21 @@ const classSchema = new mongoose.Schema({
     startTime: String, // Format: "09:00"
     endTime: String,   // Format: "10:30"
     room: String
+  },
+  settings: {
+    joinLocation: {
+      type: String,
+      enum: ['college_only', 'anywhere'],
+      default: 'college_only'
+    },
+    allowLateJoin: {
+      type: Boolean,
+      default: true
+    },
+    requireApproval: {
+      type: Boolean,
+      default: false
+    }
   }
 }, {
   timestamps: true
@@ -41,6 +68,30 @@ const classSchema = new mongoose.Schema({
 
 // Index for faster queries
 classSchema.index({ teacher: 1 });
-classSchema.index({ classGroup: 1 });
+classSchema.index({ classCode: 1 });
+classSchema.index({ students: 1 });
+
+// Generate unique class code
+classSchema.statics.generateClassCode = async function() {
+  let code;
+  let exists = true;
+  
+  while (exists) {
+    // Generate 6-character alphanumeric code
+    code = Math.random().toString(36).substring(2, 8).toUpperCase();
+    const existingClass = await this.findOne({ classCode: code });
+    exists = !!existingClass;
+  }
+  
+  return code;
+};
+
+// Virtual for student count
+classSchema.virtual('studentCount').get(function() {
+  return this.students.length;
+});
+
+// Ensure virtual fields are serialized
+classSchema.set('toJSON', { virtuals: true });
 
 module.exports = mongoose.model('Class', classSchema);
